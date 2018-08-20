fda451765811c236f78dfd05b8d768b16281a9e6
Thiago Soares <thiagomullet@gmail.com>
2018-08-20 16:08:30 -0300

    Renamed namespace

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace puc_tcc
+namespace PucTcc
 {
     public class Program
     {
