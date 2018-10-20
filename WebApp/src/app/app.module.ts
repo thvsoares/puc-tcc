04379aefc79807b57e06e751355671b62b115883
Thiago Soares <thiagomullet@gmail.com>
2018-10-20 17:04:18 -0300

    Classrooms and courses components

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { AlunosComponent } from './alunos/alunos.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { TurmasComponent } from './turmas/turmas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlunosComponent
+    AlunosComponent,
+    CursosComponent,
+    TurmasComponent
   ],
   imports: [
     BrowserModule,
