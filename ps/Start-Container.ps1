ac3cad88cffe29e2e6cf9ebbace6b3364ebb5520
Thiago Soares <thiagomullet@gmail.com>
2018-11-11 15:24:39 -0200

    startup script adjustment for clarity

--- ps/Start-Container.ps1
+++ ps/Start-Container.ps1
@@ -1,2 +1,2 @@
-docker run -d -p 1433:1433 --name sql sql
-docker run -d -p 5000:80 --name backend backend
+docker run --name sql -d -p 1433:1433 sql
+docker run --name backend -d -p 5000:80 backend
