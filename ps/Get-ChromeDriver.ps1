f2998e4eb6a695b3b5fec9f52c806285fc96025a
Thiago Soares <thiagomullet@gmail.com>
2019-02-08 21:23:11 -0200

    New selenium test project

---
+++ ps/Get-ChromeDriver.ps1
@@ -0,0 +1,7 @@
+$lastVersion = (Invoke-WebRequest -Uri 'http://chromedriver.storage.googleapis.com/LATEST_RELEASE' -UseBasicParsing).Content
+$binPath = '..\WebApiTest\bin\Debug\netcoreapp2.1'
+if (!(Test-Path $binPath)) {
+    New-Item -Path $binPath
+}
+Invoke-WebRequest -Uri "http://chromedriver.storage.googleapis.com/$lastVersion/chromedriver_win32.zip" -OutFile "$binPath\chromedriver_win32.zip"
+Expand-Archive -Path "$binPath\chromedriver_win32.zip" -DestinationPath $binPath -Force
