e333c8b6c6920e61e9d52260b8dd629ca9779e09
Thiago Soares <thiagomullet@gmail.com>
2019-02-08 23:21:37 -0200

    start frontend container

--- ps/Start-Container.ps1
+++ ps/Start-Container.ps1
@@ -1,4 +1,6 @@
 docker rm -f $(docker ps -aq -f name=sql)
 docker rm -f $(docker ps -aq -f name=backend)
+docker rm -f $(docker ps -aq -f name=frontend)
 docker run --name sql -d -p 1433:1433 sql
 docker run --name backend -d -p 5000:80 backend
+docker run --name frontend -d -p 80:80 frontend
