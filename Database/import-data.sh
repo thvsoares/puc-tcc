1e657e62f77c56bfaef6ba5bebf7532b83695514
Thiago Soares <thiagomullet@gmail.com>
2019-02-12 23:36:26 -0200

    fix to run with cr lf on linux

--- Database/import-data.sh
+++ Database/import-data.sh
@@ -1,8 +1,4 @@
 #wait for the SQL Server to come up
-sleep 15s
-
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 0WFVJ3ADrAAj -d master -i Create.sql
-
-#run the data script
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 0WFVJ3ADrAAj -d master -i InitialData.sql
+#run the intial data script
+sleep 15s; /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 0WFVJ3ADrAAj -d master -i Create.sql; /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 0WFVJ3ADrAAj -d master -i InitialData.sql;
